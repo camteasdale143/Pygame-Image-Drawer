d6f590b5399b489b7a7bb3eae92ae2d708e279bb
Cameron Teasdale <cameron.teasdale@shopify.com>
2019-09-18 17:16:05 -0400

    fix polygon colors and commmenting

--- assignment_script.py
+++ assignment_script.py
@@ -2,20 +2,11 @@ import pygame
 
 surface = pygame.display.set_mode((500,500))
 surface.fill((0, 0, 0))
-#Rectangle drawn with color:  Nebula Green - (89, 232, 23, 255)
-pygame.draw.rect(surface, (89, 232, 23, 255), pygame.Rect(133 - 50, 145, 377, 387))
+# Polygon drawn with color: White, (255, 255, 255)
+pygame.draw.polygon(surface, (255, 255, 255), [(203, 246), (158, 298), (174, 340), (220, 340), (269, 316), (228, 277), (203, 277)])
 
-#Rectangle drawn with color:  Lemon Chiffon - (255, 248, 198, 255)
-pygame.draw.rect(surface, (255, 248, 198, 255), pygame.Rect(139 - 50, 284, 29, 27))
-
-#Rectangle drawn with color:  Lemon Chiffon - (255, 248, 198, 255)
-pygame.draw.rect(surface, (255, 248, 198, 255), pygame.Rect(246 - 50, 303, 61, 40))
-
-#Rectangle drawn with color:  Blonde - (251, 246, 217, 255)
-pygame.draw.rect(surface, (251, 246, 217, 255), pygame.Rect(199 - 50, 355, 41, 134))
-
-#Circle drawn with color:  Blue Dress - (21, 125, 236, 255)
-pygame.draw.circle(surface, (21, 125, 236, 255), (404, 97), 84)
+# Polygon drawn with color: White, (255, 255, 255)
+pygame.draw.polygon(surface, (255, 255, 255), [(192, 264), (264, 112), (328, 183), (356, 216), (425, 122), (253, 58), (178, 177)])
 
 
 pygame.display.update()
--- python_image_gui.py
+++ python_image_gui.py
@@ -191,24 +191,27 @@ while running:
 
 def write_drawing(): 
     drawString = ""
-    print(backgroundColor)
     drawString += "surface.fill({0})\n".format(backgroundColor)
     for s in order:
         if s[0] == "rect":
             rectangle = rects[s[1]]
-            drawString += "#Rectangle drawn with color: {0} - {1}\n".format(rectangle[1], rectangle[0][1])
+            if (rectangle[0][1] == (255,0,0)):
+                drawString += "# Rectangle drawn with color: Red - (255, 0, 0)\n"
+            else:
+                drawString += "# Rectangle drawn with color: {0} - {1}\n".format(rectangle[1], rectangle[0][1])
             drawString += "pygame.draw.rect(surface, {0}, pygame.Rect({1} - 50, {2}, {3}, {4}))\n\n".format(rectangle[0][1], rectangle[0][2][0],rectangle[0][2][1], rectangle[0][2][2], rectangle[0][2][3])
         if s[0] == "circ":
             circle = circs[s[1]]
-            drawString += "#Circle drawn with color: {0} - {1}\n".format(circle[1], circle[0][1])
+            drawString += "# Circle drawn with color: {0} - {1}\n".format(circle[1], circle[0][1])
             drawString += "pygame.draw.circle(surface, {0}, {1}, {2})\n\n".format(circle[0][1], (circle[0][2][0] - 50, circle[0][2][1]), circle[0][3])
         if s[0] == "elli":
             ellipse = ellis[s[1]]
-            drawString += "#Ellipse drawn with color: {0} - {1}\n".format(ellipse[1], ellipse[0][1])
+            drawString += "# Ellipse drawn with color: {0} - {1}\n".format(ellipse[1], ellipse[0][1])
             drawString +=  "pygame.draw.ellipse(surface, {0}, pygame.Rect({1} - 50, {2}, {3}, {4}))\n\n".format(ellipse[0][1], ellipse[0][2][0],ellipse[0][2][1], ellipse[0][2][2], ellipse[0][2][3])
         if s[0] == "poly":
             polygon = polys[s[1]]
-            drawString += "#Rectangle drawn with color: {0}\n".format(polygon[1], polygon[0][1])
+            print(polygon[0][1])
+            drawString += "# Polygon drawn with color: {0}, {1}\n".format(polygon[1], polygon[0][1])
             mapped_points = []
             for point in range(len(polygon[0][2])):
                 mapped_points.append((polygon[0][2][point][0] - 50,polygon[0][2][point][1]))
